b2b3b8588774262b1885b43468b6f3bae7b62325
Andy <mindovermiles262@gmail.com>
2018-06-06 21:13:30 -0700

    Create trackpad.sh

---
+++ trackpad.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Script automates settings for Trackpad sensitivity using
+# xinput. Works with Dell XPS 13 9730 using libinput.
+# Place script file in /bin/init.d/ folder to run on system startup
+
+xinput set-prop 'DLL082A:01 06CB:76AF Touchpad' 'libinput Natural Scrolling Enabled' 1
+xinput set-prop 'DLL082A:01 06CB:76AF Touchpad' 'libinput Accel Speed' 0.5
