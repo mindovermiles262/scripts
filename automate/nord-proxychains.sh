89f834c68d65bcc12aa2d80d93102e6a359257b4
Andy D <mindovermiles262@gmail>
2018-11-18 19:15:22 -0800

    add proxychains script

---
+++ automate/nord-proxychains.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Quickly set up list of Proxy Servers for Proxychains
+# Uses NORD VPN as a Proxy Provider
+#
+# Copy output to [ProxyList] area of /etc/proxychains.conf
+# 
+# Be sure to change username & password
+
+DOMAIN="us"
+LOW=2100
+HIGH=2150
+
+USERNAME="user@example.com"
+PASSWORD="yoursecretpassword"
+PORT="1080"
+
+for (( c=$LOW; c<=$HIGH; c++ ))
+do
+  LOOKUP="$DOMAIN$c.nordvpn.com"
+  IP="$(nslookup $LOOKUP | grep Address | cut -d ' ' -f 2 | sed -n 2p)"
+  echo "socks5 $IP $PORT $USERNAME $PASSWORD"
+done
+
