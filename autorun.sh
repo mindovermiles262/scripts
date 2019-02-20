40adbb4ad7cdb494525c954de29b48d9e62904a3
Andy D <mindovermiles262@gmail>
2019-02-20 09:06:40 -0800

    update some things

--- autorun.sh
+++ autorun.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # Set bash_profile
-ln -s ./bash/bash_profile ~/.bash_profile
+if [ -f ~/.bash_profile ]; then
+	printf "Deleting bash_profile ... "
+	rm ~/.bash_profile
+	printf "Done.\n"
+fi
+ln -s ~/Documents/scripts/bash/bash_profile ~/.bash_profile
 
 # Install basics
 sudo apt-get update
@@ -12,17 +17,24 @@ sudo apt-get install -y curl git nodejs gcc make libssl-dev libreadline-dev zlib
 
 # Install and Initiate VIM
 sudo apt-get install -y vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -s ./vim/vimrc ~/.vimrc
-vim +PlugInstall +qall
+if [ -f ~/.vimrc ]; then
+	printf "Deleting vimrc ... "
+	rm ~/.vimrc
+	printf "Done.\n"
+fi
+if [ -d ~/.vim ]; then
+	printf "Deleting vim directory ... "
+	rm -rf ~/.vim
+	printf "Done.\n"
+fi
 
-# Add Source Code Pro font
-sudo mkdir /usr/share/fonts/truetype/sourcecodepro
-sudo cp ./data/SourceCodeVariable* /usr/share/fonts/truetype/sourcecodepro/
-sudo fc-cache -fv
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+ln -s ~/Documents/scripts/vim/vimrc ~/.vimrc
+vim +PlugInstall +qall
 
 # Add ASDF
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.6.1
+source ~/.bash_profile
 asdf plugin-add ruby
 asdf plugin-add nodejs
 
