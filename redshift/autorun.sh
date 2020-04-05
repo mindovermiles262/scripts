6c7bd2eaf0ecd726a97d1fb3543999e4bd2a8617
Andy <mindovermiles262@gmail.com>
2020-04-05 09:20:40 -0700

    Update autorun.sh

--- redshift/autorun.sh
+++ redshift/autorun.sh
@@ -17,6 +17,16 @@ DIR="$(dirname $0)"
 cp "$PWD/$DIR/Redshift.template" "$PWD/$DIR/Redshift.desktop"
 echo "Exec=/usr/bin/redshift -c $HOME/.config/redshift.conf" >> "$PWD/$DIR/Redshift.desktop"
 
+# Symlink config file
+if [[ ! -d "$HOME/.config" ]]; then
+  mkdir -p "$HOME/.config"
+fi
 ln -sf "$PWD/$DIR/redshift.conf" "$HOME/.config"
+
+# Autostart Redshift on login
+AUTOSTART_DIR="$HOME/.config/autostart"
+if [[ ! -d "$AUTOSTART_DIR" ]]; then
+  mkdir -p "$AUTOSTART_DIR"
+fi
 ln -sf "$PWD/$DIR/Redshift.desktop" "$HOME/.config/autostart"
 echo "Done"
