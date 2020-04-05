ad775a9da76391faac63d8e5df1da8c1abaa1e54
Andy D <mindovermiles262@gmail>
2020-04-04 17:35:13 -0700

    copy, dont link panel

--- xfce4-panel/autorun.sh
+++ xfce4-panel/autorun.sh
@@ -15,5 +15,9 @@ XFCE4_PANEL_PATH="$HOME/.config/xfce4/xfconf/xfce-perchannel-xml/"
 XFCE4_PANEL_FILE="xfce4-panel.xml"
 PANEL_FULL_PATH="$XFCE4_PANEL_PATH$XFCE4_PANEL_FILE"
 
-ln -sf "$PWD/$DIR/xfce4-panel.xml" "$PANEL_FULL_PATH"
+if [[ -f "$PANEL_FULL_PATH" ]]; then
+  mv "$PANEL_FULL_PATH" "$PANEL_FULL_PATH.bak"
+fi
+
+cp "$PWD/$DIR/xfce4-panel.xml" "$PANEL_FULL_PATH"
 echo "Done."
