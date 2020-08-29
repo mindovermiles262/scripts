4641dbaa293e83174f0165d9ad2ebdb781ca5e67
Andy D <mindovermiles262@gmail>
2020-08-29 15:56:58 -0500

    first submodule, moved all vim stuff to vim folder

---
+++ vim.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export TARGETENV="fedora"
+DIR="$(dirname $0)"
+
+if [[ ! "$DIR" == "." ]]; then
+  echo "[!] ERROR: Must be run from project root directory"
+  exit 1
+fi
+
+"$DIR/vim/autorun.sh"
