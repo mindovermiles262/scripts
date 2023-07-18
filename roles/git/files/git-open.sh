cd2131248826365ea98c5a5da038eb64ba5a757b
Andrew Duss <andrew.duss@storable.com>
2023-07-18 08:59:21 -0600

    feat: added git open command
    Signed-off-by: Andrew Duss <andrew.duss@storable.com>

---
+++ roles/git/files/git-open.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then BRANCH="origin"; else BRANCH="$1"; fi
+REMOTE=$(git config --get remote.$BRANCH.url)
+if [[ "$REMOTE" =~ "git@" ]]; then
+  URL=$(echo "$REMOTE" | awk -F "@" '{print $2}' | awk -F " " '{print $1}' | sed 's/:/\//g' | sed 's/.git//g' | awk '{print "https://"$1}')
+else
+  URL=$REMOTE
+fi
+echo "[*] $URL"
+open "$URL"
+
