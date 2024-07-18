2a330aa2b7beff983c8b1c9b4ee1994b8a87feea
Andrew Duss <aduss@villahomes.com>
2024-07-18 09:47:14 -0600

    feat: change PS1, add dennas mount script
    Signed-off-by: Andrew Duss <aduss@villahomes.com>

---
+++ scripts/mount-dennas.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+MNT_DIR=/mnt/dennas
+CRD=/home/dipsea/.credentials_denhac
+NAS=dennas.local
+SHARE=denNAS/denhac_common
+
+if [ "$EUID" -ne 0 ];
+then
+  echo "[!] Must be run as root."
+  exit 1
+fi
+
+if [ ! -d "$MNT_DIR" ];
+then
+  echo "[!] Mount directory does not exist. Creating $MNT_DIR"
+  mkdir -p $MNT_DIR
+fi
+
+echo "[*] Mounting $NAS/$SHARE to $MNT_DIR"
+
+mount -t cifs -o credentials=$CRD,uid=1000,gid=1000 //$NAS/$SHARE $MNT_DIR
+
+ls -l $MNT_DIR
+
