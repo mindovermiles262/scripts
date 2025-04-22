50e963e95f7a1a33ae0d4809f999f667b8ba42af
Andrew <mindovermiles262@gmail>
2025-04-22 09:24:15 -0600

    feat: update
    Signed-off-by: Andrew <mindovermiles262@gmail>

--- scripts/mount-dennas.sh
+++ scripts/mount-dennas.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-MNT_DIR=/mnt/dennas
-CRD=/home/dipsea/.credentials_denhac
-NAS=dennas.local
+MNT_DIR=/media/dennas
+# CRD=/home/dipsea/.credentials_denhac
+NAS=DENNAS
 SHARE=denNAS/denhac_common
 
 if [ "$EUID" -ne 0 ];
@@ -19,7 +19,8 @@ fi
 
 echo "[*] Mounting $NAS/$SHARE to $MNT_DIR"
 
-mount -t cifs -o credentials=$CRD,uid=1000,gid=1000 //$NAS/$SHARE $MNT_DIR
+# mount -t cifs -o credentials=$CRD,uid=1000,gid=1000 //$NAS/$SHARE $MNT_DIR
+mount -t cifs -o uid=1000,gid=1000 //$NAS/$SHARE $MNT_DIR
 
 ls -l $MNT_DIR
 
