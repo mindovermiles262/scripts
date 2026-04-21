58f5be87a2f45cc8128d964200ddc974e1d520eb
Andrew <mindovermiles262@gmail>
2026-04-21 12:26:38 -0700

    chore: update linux
    Signed-off-by: Andrew <mindovermiles262@gmail>

--- scripts/sync-ipod.sh
+++ scripts/sync-ipod.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 usage() {
   echo ""
   echo "Usage"
@@ -13,6 +15,30 @@ if [[ $# -ne 1 ]]; then
   exit 1
 fi
 
-rsync -rv --size-only --delete "$HOME/Music/Library/* $1/Music"
-rsync -rv --size-only --delete "$HOME/Music/Audiobooks/* $1/Audiobooks"
-rsync -rv --size-only --delete "$HOME/Music/Podcasts/* $1/Podcasts"
+# Copy Music
+rsync --dry-run -rv --size-only "$HOME/Music/Library/"* "$1/Music"
+read -n 1 -p "Look good? [y/n]: " MUSIC_CONFIRM
+echo ""
+
+if [ "$MUSIC_CONFIRM" = "Y" ] || [ "$MUSIC_CONFIRM" = "y" ]; then
+  rsync -rv --size-only "$HOME/Music/Library/"* "$1/Music"
+else
+  echo "Confirmation was not 'y' ... skipping Music sync"
+  sleep 2
+fi
+
+# Copy Podcasts
+rsync --dry-run -rv --size-only "$HOME/Music/Podcasts/"* "$1/Podcasts"
+read -n 1 -p "Look good? [y/n]: " POD_CONFIRM
+echo ""
+
+if [ "$POD_CONFIRM" = "Y" ] || [ "$POD_CONFIRM" = "y" ]; then
+  rsync -rv --size-only "$HOME/Music/Podcasts/"* "$1/Podcasts"
+else
+  echo "Confirmation was not 'y' ... skipping Podcast sync"
+  sleep 2
+fi
+
+# TODO: Copy Audiobooks
+# rsync -rv --size-only --delete "$HOME/Music/Audiobooks/* $1/Audiobooks"
+# rsync -rv --size-only --delete "$HOME/Music/Podcasts/* $1/Podcasts"
